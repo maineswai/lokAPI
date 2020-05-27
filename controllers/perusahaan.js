0ecc8b514e065bcf843aeab267124906aa513f37
Febrian <solihinfebrian0@gmail.com>
2020-05-27 12:54:52 +0700

    tambah status code & message di result json

--- controllers/perusahaan.js
+++ controllers/perusahaan.js
@@ -3,11 +3,18 @@ const models = require("../database/models");
 const createPerusahaan = async (req, res) => {
     try {
         const perusahaan = await models.Perusahaan.create(req.body);
+        // return res.status(201).json({ perusahaan });
         return res.status(201).json({
-            perusahaan,
+            status: 201,
+            message: "Perusahaan successfully created",
+            data: perusahaan
         });
     } catch (error) {
-        return res.status(500).json({error: error.message})
+        // return res.status(500).json({error: error.message})
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        });
     }
 }
 
@@ -27,9 +34,18 @@ const getAllPerusahaan = async (req, res) => {
                 }
             ]
         });
-        return res.status(200).json({ perusahaan });
+        // return res.status(200).json({ perusahaan });
+        return res.json({
+            status: 200,
+            message: "Perusahaan successfully displayed",
+            data: perusahaan
+        });
     } catch (error) {
-        return res.status(500).send(error.message);
+        // return res.status(500).send(error.message);
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        });
     }
 }
 
@@ -41,11 +57,24 @@ const getPerusahaanById = async (req, res) => {
             include: [],
         });
         if (perusahaan) {
-            return res.status(200).json({ perusahaan });
+            // return res.status(200).json({ perusahaan });
+            return res.status(200).json({
+                status: 200,
+                message: "Perusahaan successfully displayed",
+                data: perusahaan
+            });
         }
-        return res.status(404).send("Perusahaan with the specified ID does not exists");
+        // return res.status(404).send("Perusahaan with the specified ID does not exists");
+        return res.status(404).json({
+            status: 404,
+            message: "Perusahaan with the specified ID does not exists"
+        });
     } catch (error) {
-        return res.status(500).send(error.message);
+        // return res.status(500).send(error.message);
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        });
     }
 }
 
@@ -57,11 +86,20 @@ const updatePerusahaan = async (req, res) => {
         });
         if (updated) {
             const updatedPerusahaan = await models.Perusahaan.findOne({ where: { id: perusahaanId } });
-            return res.status(200).json({ perusahaan: updatedPerusahaan });
+            // return res.status(200).json({ perusahaan: updatedPerusahaan });
+            return res.status(200).json({
+                status: 200,
+                message: "Perusahaan successfully updated",
+                data: updatedPerusahaan
+            });
         }
         throw new Error("Perusahaan not found");
     } catch (error) {
-        return res.status(500).send(error.message);
+        // return res.status(500).send(error.message);
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        });
     }
 }
 
@@ -72,11 +110,19 @@ const deletePerusahaan = async (req, res) => {
             where: { id: perusahaanId }
         });
         if (deleted) {
-            return res.status(204).send("Perusahaan deleted");
+            // return res.status(204).send("Perusahaan deleted");
+            return res.status(204).json({
+                status: 204,
+                message: "Perusahaan successfully deleted"
+            });
         }
         throw new Error("Perusahaan not found");
     } catch (error) {
-        return res.status(500).send(error.message);
+        // return res.status(500).send(error.message);
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        });
     }
 }
 
